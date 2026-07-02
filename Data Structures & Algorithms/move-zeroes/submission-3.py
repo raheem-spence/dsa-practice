7cc74faa8fb08ff57f06e3be191e9e345c50faf4
Raheem Spence <heem1284@gmail.com>
2026-07-02 08:11:36 -0400

    Add: move-zeroes - submission-3

---
+++ Data Structures & Algorithms/move-zeroes/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # given: array nums
+        # goal: move all 0's to the end of array while maintaining relative
+        # order of non-zero elements
+        # pattern: 
+        # approach: use two pointers??
+
+        l = 0
+        r = 0
+
+        while r < len(nums):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
