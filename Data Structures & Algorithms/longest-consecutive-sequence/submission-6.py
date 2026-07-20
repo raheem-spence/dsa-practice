f78010351df37222fb0626c0c1a3f5bff860de29
Raheem Spence <heem1284@gmail.com>
2026-07-20 14:09:40 -0400

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # given: array of integers nums
+        # goal: return length of longest consecutive subsequence that can be formed
+        # where a consecutive sequence is a sequence of elements in which each element is
+        # 1 greater than the previous elements
+        # pattern: ?
+        # approach: ?
+
+        # [2, 20, 4, 10, 3, 5]
+
+        longest_seq = 0
+        nums_set = set(nums)
+
+        for num in nums_set:
+            if num - 1 not in nums_set:
+                length = 1
+
+                while num + length in nums_set:
+                        length += 1
+                longest_seq = max(longest_seq, length)
+
+        return longest_seq
+
