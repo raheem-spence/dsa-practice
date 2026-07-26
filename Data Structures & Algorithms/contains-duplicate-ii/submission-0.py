0c1d712ccf0fa5fc4c6ca595c42d79a106a403bb
Raheem Spence <heem1284@gmail.com>
2026-07-26 16:26:56 -0400

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # given: integer array `nums` and an integer `k`
+        # goal: return true if there are two distinct indices `i` and `j` in the array such that
+        # `nums[i] == nums[j]` and `abs(i - j) <= k, otherwise false
+        # pattern: arrays and hashing
+        # approach: use a hash map to see store 
+
+
+        num_map = {}
+
+        for i, num in enumerate(nums):
+            if num in num_map:
+                if abs(i - num_map[num]) <= k:
+                    return True
+            num_map[num] = i
+        
+        return False
+
+
+
+        
