a716e7dcf9f2aa463ed305a7ecadc8e9d6348379
Raheem Spence <heem1284@gmail.com>
2026-08-09 19:47:12 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # given: a list of strings 
+        # goal: design an algorithm to encode to a string 
+        # approach: we can use a delimiter that sits between strings and a number as well
+
+        encoded_string = ""
+        for s in strs:
+            encoded_string += str(len(s)) + "#" + s
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        decoded_strs = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[i:j])
+            string = ""
+            i = j + 1
+            while length > 0:
+                string += s[i]
+                length -= 1
+                i += 1
+            decoded_strs.append(string)
+
+
+        return decoded_strs
